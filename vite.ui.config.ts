5ba99a2dbd0d9426d85c4349ef37b76f057881f1
Valen44 <valenmercado44@gmail.com>
2025-08-05 23:14:35 -0300

    combined dashboard and popup build

--- vite.ui.config.ts
+++ vite.ui.config.ts
@@ -11,6 +11,7 @@ export default defineConfig({
     rollupOptions: {
       input: {
         dashboard: resolve(__dirname, 'src/dashboard/dashboard.html'),
+        popup: resolve(__dirname, 'src/popup/popup.html'),
         preloadTheme: resolve(__dirname, 'src/dashboard/preloadtheme.ts'),
       },
       output: {
